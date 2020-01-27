710ca6ce577d376d920ff6dfbf1a3c4b025cab54
MinimalistCat2019 <48767239+MinimalistCat2019@users.noreply.github.com>
2020-01-27 15:12:10 +0000

    remove burger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
 import * as api from '../api';
-import { slide as Menu } from "react-burger-menu";
 
 class NavBar extends Component {
     
@@ -25,14 +24,12 @@ class NavBar extends Component {
     render() {
         if (this.state.isLoading === true) return <p>Loading...</p>
         return (
-            <Menu>
             <nav className="navbar">
             <Link to={`/`} className="menu-item"><h4>home</h4></Link>
             {this.state.topics.map(topic => (
             <Link to={`/${topic.slug}`} key={topic.slug} className="menu-item"><h4>{topic.slug}</h4></Link>
             ))}
             </nav>
-            </Menu>
         );
     }
 }
